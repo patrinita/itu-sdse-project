83a17a4d200f195324a94ca330b5a66a099f14c2
amina <aminalykkesaid01@gmail.com>
2025-12-08 15:50:31 +0100

    feat: setup structure for pipeline.mod
    Co-authored-by: patrinita <patricia1302@yahoo.com>

---
+++ dagger/pipeline.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// Create a shared background context
+
+	// Run pipeline stages
+	stage()
+}
+
+func stage() {
+	// Create a Dagger client
+	// Define the code you want to run
+}
