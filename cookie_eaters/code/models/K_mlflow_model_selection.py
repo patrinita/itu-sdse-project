5ae029fcf5e2c293e7e283eb6cb39f4092c6366a
Patricia Nita <patricia_nita@yahoo.com>
2025-12-11 17:00:57 +0100

    refactor: defined functions, added main()

--- cookie_eaters/code/models/K_mlflow_model_selection.py
+++ cookie_eaters/code/models/K_mlflow_model_selection.py
@@ -2,7 +2,6 @@ import json
 import time
 from mlflow.tracking.client import MlflowClient
 from mlflow.entities.model_registry.model_version_status import ModelVersionStatus
-from mlflow.tracking.client import MlflowClient
 
 def wait_until_ready(model_name, model_version):
     client = MlflowClient()
@@ -18,21 +17,34 @@ def wait_until_ready(model_name, model_version):
         time.sleep(1)
         
 #get experiment model results
-experiment_ids = [mlflow.get_experiment_by_name(experiment_name).experiment_id]
-experiment_ids
-
-experiment_best = mlflow.search_runs(
-    experiment_ids=experiment_ids,
-    order_by=["metrics.f1_score DESC"],
-    max_results=1
-).iloc[0]
-experiment_best
+def get_experiment_best(experiment_name):#added
+    experiment_ids = [mlflow.get_experiment_by_name(experiment_name).experiment_id]
+    experiment_best = mlflow.search_runs(
+        experiment_ids=experiment_ids,
+        order_by=["metrics.f1_score DESC"],
+        max_results=1
+    ).iloc[0]
+    return experiment_best#changed
 
 #metrics
-with open("./artifacts/model_results.json", "r") as f:
-    model_results = json.load(f)
-results_df = pd.DataFrame({model: val["weighted avg"] for model, val in model_results.items()}).T
-results_df
+def load_model_results(path="./artifacts/model_results.json"):#added
+    with open(path, "r") as f:
+        model_results = json.load(f)
+    results_df = pd.DataFrame({model: val["weighted avg"] for model, val in model_results.items()}).T
+    return model_results, results_df #changed
 
-best_model = results_df.sort_values("f1-score", ascending=False).iloc[0].name
-print(f"Best model: {best_model}")
+def get_best_model_name(results_df):#added
+    best_model = results_df.sort_values("f1-score", ascending=False).iloc[0].name
+    return best_model#changed
+
+def main():#added all lines below:
+    experiment_best = get_experiment_best(experiment_name)
+
+    model_results, results_df = load_model_results("./artifacts/model_results.json")
+
+    best_model = get_best_model_name(results_df)
+    print(f"Best model: {best_model}")
+
+
+if __name__ == "__main__":
+    main()
