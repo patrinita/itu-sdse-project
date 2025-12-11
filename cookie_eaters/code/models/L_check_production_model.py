f783cb0bcfd484907aa3a1dec63c2b497dbbeb0c
Patricia Nita <patricia_nita@yahoo.com>
2025-12-11 17:12:00 +0100

    refactor: defined functions, added main()

--- cookie_eaters/code/models/L_check_production_model.py
+++ cookie_eaters/code/models/L_check_production_model.py
@@ -1,17 +1,29 @@
 from mlflow.tracking import MlflowClient
 
 #production model
-client = MlflowClient()
-prod_model = [model for model in client.search_model_versions(f"name='{model_name}'") if dict(model)['current_stage']=='Production']
-prod_model_exists = len(prod_model)>0
+def get_production_model(model_name):#added
+    client = MlflowClient()
+    prod_model = [model for model in client.search_model_versions(f"name='{model_name}'") if dict(model)['current_stage']=='Production']
+    return prod_model#changed
 
-if prod_model_exists:
-    prod_model_version = dict(prod_model[0])['version']
-    prod_model_run_id = dict(prod_model[0])['run_id']
+def evaluate_production_model(prod_model, model_name):#added
+    prod_model_exists = len(prod_model)>0
+
+    if prod_model_exists:
+        prod_model_version = dict(prod_model[0])['version']
+        prod_model_run_id = dict(prod_model[0])['run_id']
+        
+        print('Production model name: ', model_name)
+        print('Production model version:', prod_model_version)
+        print('Production model run id:', prod_model_run_id)
     
-    print('Production model name: ', model_name)
-    print('Production model version:', prod_model_version)
-    print('Production model run id:', prod_model_run_id)
-    
-else:
-    print('No model in production')
+    else:
+        print('No model in production')
+
+def main():#added all lines below
+    prod_model = get_production_model(model_name)
+    evaluate_production_model(prod_model, model_name)
+
+
+if __name__ == "__main__":
+    main()
