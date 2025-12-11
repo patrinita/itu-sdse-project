32a79e21f0acc0fb7c5719104da7d2810e506186
Patricia Nita <patricia_nita@yahoo.com>
2025-12-11 15:57:45 +0100

    refactor: defined functions, added main()

--- cookie_eaters/code/models/10_save_artifacts.py
+++ cookie_eaters/code/models/10_save_artifacts.py
@@ -1,12 +1,26 @@
+import json #added
+from pprint import pprint #added
+
 #save the model result
-column_list_path = './artifacts/columns_list.json'
-with open(column_list_path, 'w+') as columns_file:
-    columns = {'column_names': list(X_train.columns)}
-    pprint(columns)
-    json.dump(columns, columns_file)
+def save_column_list(X_train, path):#added
+    with open(path, 'w+') as columns_file:
+        columns = {'column_names': list(X_train.columns)}
+        pprint(columns)
+        json.dump(columns, columns_file)
+    print('Saved column list to', path)
 
-print('Saved column list to ', column_list_path)
+def save_model_results(model_results, path):
+    with open(path, 'w+') as results_file:
+        json.dump(model_results, results_file)
+    print('Saved model results to', path)
 
-model_results_path = "./artifacts/model_results.json"
-with open(model_results_path, 'w+') as results_file:
-    json.dump(model_results, results_file)
+def main():
+    column_list_path = './artifacts/columns_list.json'
+    model_results_path = './artifacts/model_results.json'
+
+    save_column_list(X_train, column_list_path)
+    save_model_results(model_results, model_results_path)
+
+
+if __name__ == "__main__":
+    main()
