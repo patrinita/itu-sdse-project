2bc1f177381f720e506cc1ab5d08de728461be68
amina <aminalykkesaid01@gmail.com>
2025-12-12 20:09:49 +0100

    refactor:restructering defined functions inn H_evaluation.py

--- cookie_eaters/code/models/H_evaluation.py
+++ cookie_eaters/code/models/H_evaluation.py
@@ -2,50 +2,41 @@ from pprint import pprint
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
-import json
+import pandas as pd
 
 
 #test accuracy
-def print_best_params(model_grid): #added this one
+def best_params(model_grid): #added this one
+
     best_model_xgboost_params = model_grid.best_params_
-    print("Best xgboost params")
-    pprint(best_model_xgboost_params)
+    
+    return best_model_xgboost_params
 
 def get_predictions(model_grid, X_train, X_test): #added
+
     y_pred_train = model_grid.predict(X_train)
     y_pred_test = model_grid.predict(X_test)
+
     return y_pred_train, y_pred_test #added
-def print_accuracy(y_train, y_test, y_pred_train, y_pred_test): #added
-    print("Accuracy train", accuracy_score(y_pred_train, y_train ))
-    print("Accuracy test", accuracy_score(y_pred_test, y_test))
 
+#look at this again because conf_matrix is double...
 #performance overview
-
-def performance_test(model_grid, X_test, y_test): #added
+def performance(model_grid, X_test, y_test, X_train, y_train): #added
+    
     y_pred_test = model_grid.predict(X_test)#added
-
     conf_matrix = confusion_matrix(y_test, y_pred_test)
-    print("Test actual/predicted\n")
-    print(pd.crosstab(y_test, y_pred_test, rownames=['Actual'], colnames=['Predicted'], margins=True),'\n')
-    print("Classification report\n")
-    print(classification_report(y_test, y_pred_test),'\n')
-return y_pred_test #added
 
-def performance_train(model_grid, X_train, y_train):
+    
     y_pred_train = model_grid.predict(X_train)
     conf_matrix = confusion_matrix(y_train, y_pred_train)
-    print("Train actual/predicted\n")
-    print(pd.crosstab(y_train, y_pred_train, rownames=['Actual'], colnames=['Predicted'], margins=True),'\n')
-    print("Classification report\n")
-    print(classification_report(y_train, y_pred_train),'\n')
-    return y_pred_train #added
 
-def print_accuracy(y_train, y_test, y_pred_train, y_pred_test):#added all 3 lines
-    print("Accuracy train", accuracy_score(y_pred_train, y_train))
-    print("Accuracy test", accuracy_score(y_pred_test, y_test))
+    
+    return y_pred_train, y_pred_test #added
+
 
 #save the best model
 def save_best_model_and_results(model_grid, y_train, y_pred_train): #added
+    
     xgboost_model = model_grid.best_estimator_
     xgboost_model_path = "./artifacts/lead_model_xgboost.json"
     xgboost_model.save_model(xgboost_model_path)
@@ -56,15 +47,28 @@ def save_best_model_and_results(model_grid, y_train, y_pred_train): #added
     return model_results #added
 
 def main():#added all lines below
-    print_best_params(model_grid)
-
-    y_pred_test = performance_test(model_grid, X_test, y_test)
-    y_pred_train = performance_train(model_grid, X_train, y_train)
-
-    print_accuracy(y_train, y_test, y_pred_train, y_pred_test)
-
+    best_params(model_grid)
+        # ###add the print statements
+    # print("Best xgboost params")
+    # pprint(best_model_xgboost_params)
+    get_predictions(model_grid, X_train, X_test)
+    
+    # print("Accuracy train", accuracy_score(y_pred_train, y_train ))
+    # print("Accuracy test", accuracy_score(y_pred_test, y_test))
+    # print("Accuracy train", accuracy_score(y_pred_train, y_train))
+    # print("Accuracy test", accuracy_score(y_pred_test, y_test))
+    # ###
+    performance(model_grid, X_test, y_test, X_train, y_train)
+    print("Test actual/predicted\n")
+    print(pd.crosstab(y_test, y_pred_test, rownames=['Actual'], colnames=['Predicted'], margins=True),'\n')
+    print("Classification report\n")
+    print("Train actual/predicted\n")
+    print(pd.crosstab(y_train, y_pred_train, rownames=['Actual'], colnames=['Predicted'], margins=True),'\n')
+    print("Classification report\n")
+    print(classification_report(y_train, y_pred_train),'\n')
+    print(classification_report(y_test, y_pred_test),'\n')
     save_best_model_and_results(model_grid, y_train, y_pred_train)
-
+    
 
 if __name__ == "__main__":
     main()
